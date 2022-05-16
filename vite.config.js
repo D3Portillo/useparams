7ad2553b1eaf9b3c277ab7c63a142762aa0004ad
D3Portillo <d3portillo@gmail.com>
2022-05-16 18:57:01 +0200

    enh(vite.config.js): enhance package minify

--- vite.config.js
+++ vite.config.js
@@ -6,20 +6,26 @@ export default defineConfig({
   build: {
     target: "esnext",
     lib: {
+      formats: ["es", "umd"],
       name: packageJson.name,
       entry: path.resolve(__dirname, "lib/index.ts"),
       fileName: (format) => `index.${format}.js`,
     },
     minify: "esbuild",
-    chunkSizeWarningLimit: 2,
+    chunkSizeWarningLimit: 1,
     rollupOptions: {
-      external: ["react", "react-dom"],
+      external: ["react"],
       output: {
         globals: {
           react: "React",
-          "react-dom": "ReactDOM",
         },
       },
     },
+  },
+  esbuild: {
+    minify: true,
+    minifySyntax: true,
+    minifyIdentifiers: true,
+    minifyWhitespace: true,
   },
 });
